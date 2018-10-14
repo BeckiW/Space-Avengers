039e236fdbbef1a8762176518a4e97c22cdbb394
Becki Wordsworth <r.a.wordsworth@gmail.com>
2018-10-14 02:13:05 +0200

    Background and other setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Space-Avengers
+
 My Tjejer Kodar 2018 project!
---
+++ source/maingamestate.js
@@ -0,0 +1,38 @@
+var mainGameState = {}
+
+mainGameState.preload = function() { 
+    console.log("Pre-loading the Game");
+    this.game.load.image("space-bg", "assets/images/space-bg.jpg");
+    this.game.load.image("player-ship", "assets/player-ship.jpg" )
+    this.game.load.audio("game-music", "assets/music/maingame.mp3");
+}
+
+mainGameState.create = function() { 
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+    this.game.add.sprite(0, 0, 'space-bg');
+    
+     var shipX = this.game.width * 0.5;
+     var shipY = this.game.height * 0.90;
+
+    
+    this.cursors = this.game.input.keyboard.createCursorKeys();        
+
+    this.playerShip = this.game.add.sprite(shipX, shipY, 'player-ship');
+    this.playerShip.anchor.setTo(0.5, 0.5);
+    this.game.physics.arcade.enable(this.playerShip);
+
+
+}
+
+mainGameState.update = function() { 
+    
+     if ( this.cursors.left.isDown ) {
+        this.playerShip.body.velocity.x = -200;
+    } else if ( this.cursors.right.isDown ) {
+        this.playerShip.body.velocity.x = 200;
+    } else {
+        this.playerShip.body.velocity.x = 0;
+    }
+
+}
